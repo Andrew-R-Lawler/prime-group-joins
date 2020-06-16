28d3f54869f40e1de064e81eb546ad3222cddc90
Andrew Lawler <Andrew.R.Lawler@gmail.com>
2020-06-16 12:34:03 -0500

    base questions finished

--- joins-solution.sql
+++ joins-solution.sql
@@ -20,21 +20,29 @@ JOIN "warehouse_product" on "warehouse"."id" = "warehouse_product"."warehouse_id
 JOIN "products" on "warehouse_product"."product_id" = "products"."id"
 WHERE "products"."id" = 6;
 
-
 -- 5. Get the number of orders for each customer.
 --  NOTE: It is OK if those without orders are not included in the results.
-
-
+SELECT "first_name", "last_name", count("customers"."id") FROM "orders"
+JOIN "addresses" ON "orders"."address_id" = "addresses"."id"
+JOIN "customers" ON "addresses"."customer_id" = "customers"."id"
+GROUP BY "customers"."id"
 
 -- 6. How many customers do we have?
+SELECT count("id") FROM "customers";
 
-
+customers = 4
 
 -- 7. How many products do we carry?
+SELECT count("id") FROM "products";
 
-
+products = 7
 
 -- 8. What is the total available on-hand quantity of diet pepsi?
+SELECT SUM("on_hand") FROM "warehouse_product"
+JOIN "products" ON "warehouse_product"."product_id" = "products"."id"
+WHERE "products"."id" = 6;
+
+quantity = 92
 
 [ Stretch Goals ]
 
